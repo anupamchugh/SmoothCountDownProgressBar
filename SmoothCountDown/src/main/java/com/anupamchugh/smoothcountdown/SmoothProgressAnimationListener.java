ffb1141e2ce425028d330cdb0e524f7fe36a8cbc
anupamchugh <anupam.chugh@gmail.com>
2017-02-21 22:43:17 +0530

    Library Added

---
+++ SmoothCountDown/src/main/java/com/anupamchugh/smoothcountdown/SmoothProgressAnimationListener.java
@@ -0,0 +1,12 @@
+package com.anupamchugh.smoothcountdown;
+
+/**
+ * Created by eralpyucel on 11/12/2016.
+ */
+
+public interface SmoothProgressAnimationListener {
+
+    void onValueChanged(float value);
+
+    void onAnimationEnd();
+}
